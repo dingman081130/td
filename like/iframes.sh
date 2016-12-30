a17c35c54227dd259380f2eb514f62b90a1651d8
dingman081130 <dingman081130@gmail.com>
2016-12-30 23:08:34 +0800

    rm iframes.sh, code in iframes.sh was moved into pages.sh

--- like/iframes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-i=0
-while IFS='' read -r url || [[ -n "$url" ]]; do
-    echo "$i: $url"
-    filepath=./iframes/$i.html
-    proxychains4 wget --no-check-certificate --tries=3 -qc -T 3 -O $filepath $url &
-    i=$((i+1))
-done < ./iframes.txt
